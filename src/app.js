7c1e59fac7793d7139b9aafb4c677e3035ea21e2
MarioMarkov <mario11septemvri@gmail.com>
2021-02-13 23:59:30 +0200

    Most of the UI done

--- src/app.js
+++ src/app.js
@@ -1,4 +1,11 @@
 import { http } from './http'
 
-const data  =http.get('http://localhost:3000/comments');
-data.then( users=> console.log(users));
+document.querySelector('#userName').addEventListener('keyup',updateGreeting)
+
+function updateGreeting(e){
+
+  const greeting = document.querySelector('#greeting');
+  greeting.textContent = e.target.value
+
+  e.preventDefault();
+}
